178182edeeb449b47d514fb6ed11c86441c163f6
Ansel <anselanselmi@gmail.com>
2023-09-29 14:20:29 -0600

    Testing Bot

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from agentforge.agents.SummarizationAgent import SummarizationAgent
+
+app = Flask(__name__)
+
+summ_agent = SummarizationAgent()
+
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data = request.get_json()
+    text = data.get('text')
+    result = summ_agent.summarize(text=text)  # Replace with appropriate method call
+    return jsonify(result=result)
+
+
+if __name__ == '__main__':
+    app.run()
